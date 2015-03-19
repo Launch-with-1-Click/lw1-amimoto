d12b46551935b0dcd91e19edb6fa6b912461ac42
wokamoto <wokamoto1973@gmail.com>
2015-03-19 11:30:33 +0900

    some fix

--- recipes/nginx_default.rb
+++ recipes/nginx_default.rb
@@ -2,15 +2,19 @@
 %w{ default.conf default.backend.conf }.each do | file_name |
   template "/etc/nginx/conf.d/" + file_name do
     variables(
+      :listen => node[:nginx][:config][:listen],
+      :listen_backend => node[:nginx][:config][:listen_backend],
       :server_name => node[:ec2][:instance_id],
+      :wp_multisite => node[:nginx][:config][:wp_multisite],
+      :mobile_detect_enable => node[:nginx][:config][:mobile_detect_enable],
       :phpmyadmin_enable => node[:nginx][:config][:phpmyadmin_enable]
     )
     source "nginx/conf.d/" + file_name + ".erb"
-    notifies :reload, 'service[nginx]'
+    notifies :restart, 'service[nginx]'
   end
 end
 
-%w{ /opt/local/amimoto /opt/local/amimoto/wp-admin }.each do | dir_name |
+%w{ /opt/local/amimoto /opt/local/amimoto/wp-admin /var/www/vhosts/#{node[:ec2][:instance_id]} }.each do | dir_name |
   directory dir_name do
     owner node[:php][:config][:user]
     group node[:php][:config][:group]
@@ -25,8 +29,4 @@ template "/opt/local/amimoto/wp-admin/install.php" do
     :instance_id => node[:ec2][:instance_id]
   )
   source "install.php.erb"
-end
-
-service "nginx" do
-  action node[:nginx][:service_action]
 end
