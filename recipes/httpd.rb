674149280d44410f2844fac5259321e10cfc5e48
sawanoboly <sawanoboriyu@higanworks.com>
2016-01-19 20:32:58 +0900

    put /etc/httpd/conf.d/install_check.conf

--- recipes/httpd.rb
+++ recipes/httpd.rb
@@ -16,6 +16,14 @@ template "/etc/httpd/conf/httpd.conf" do
   end
 end
 
+template "/etc/httpd/conf.d/install_check.conf" do
+  variables node[:httpd][:config]
+  source "httpd/conf.d/install_check.conf.erb"
+  if node[:httpd][:service_action].include?(:start)
+    notifies :restart, 'service[httpd]'
+  end
+end
+
 %W{ /var/log/httpd }.each do | dir_name |
   directory dir_name do
     owner node[:httpd][:config][:user]
