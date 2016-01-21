3ab2ddb2d7dda553e20ca0ae848a72a8fcf1c391
sawanoboly <sawanoboriyu@higanworks.com>
2016-01-21 12:02:49 +0900

    include mod_remoteip

--- recipes/httpd_default.rb
+++ recipes/httpd_default.rb
@@ -1,5 +1,14 @@
 # configure httpd
 
+## compiled with apxs 2.2 on amazon linux
+cookbook_file "/usr/lib64/httpd/modules/mod_remoteip.so" do
+  source "httpd22/mod_remoteip.so"
+  owner "root"
+  group "root"
+  mode  "0755"
+end
+
+
 template "/etc/httpd/conf/httpd.conf" do
   variables node[:httpd][:config]
   source "httpd/conf/httpd.conf.erb"
@@ -11,6 +20,14 @@ end
 template "/etc/httpd/conf.d/install_check.conf" do
   variables node[:httpd][:config]
   source "httpd/conf.d/install_check.conf.erb"
+  if node[:httpd][:service_action].include?(:start)
+    notifies :restart, 'service[httpd]'
+  end
+end
+
+template "/etc/httpd/conf.d/mod_remoteip.conf" do
+  variables node[:httpd][:config]
+  source "httpd/conf.d/mod_remoteip.conf.erb"
   if node[:httpd][:service_action].include?(:start)
     notifies :restart, 'service[httpd]'
   end
