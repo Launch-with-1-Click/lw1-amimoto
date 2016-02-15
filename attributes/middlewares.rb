40240e87bbef147c7f2a9be1051c73c6b5588d3a
wokamoto <wokamoto1973@gmail.com>
2016-02-15 14:33:21 +0900

    change remi repository url and more

--- attributes/middlewares.rb
+++ attributes/middlewares.rb
@@ -119,10 +119,15 @@ default[:mysql][:config][:group] = 'mysql'
 default[:mysql][:config][:max_allowed_packet] = '1M'
 default[:mysql][:config][:innodb_buffer_pool_size] = '64M'
 default[:mysql][:config][:innodb_log_file_size] = '16M'
+default[:mysql][:config][:innodb_buffer_pool_instances] = '1'
 default[:mysql][:config][:query_cache_size] = '64M'
 default[:mysql][:config][:tmp_table_size]  = '64M'
 default[:mysql][:config][:max_connections] = '128'
 default[:mysql][:config][:thread_cache] = '128'
+default[:mysql][:config][:sort_buffer_size] = '512k'
+default[:mysql][:config][:read_buffer_size] = '256K'
+default[:mysql][:config][:read_rnd_buffer_size] = '256K'
+default[:mysql][:config][:join_buffer_size] = '256K'
 
 ## memcached
 default[:memcached][:enabled] = true
