8204e21e494a41a4b6900aba2139312e743459c8
wokamoto <wokamoto1973@gmail.com>
2016-07-01 12:02:17 +0900

    added wp-setup-hhvm-woo

---
+++ initial.wp-setup-hhvm-woo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+AMIMOTO_BRANCH='2016.01'
+
+hash jq  || /usr/bin/yum -y install jq
+
+[ ! -e /opt/local ] && mkdir -p /opt/local
+[ ! -f /opt/local/amimoto.json ] && echo '{"run_list":["recipe[amimoto]"]}' > /opt/local/amimoto.json
+
+# node[:hhvm][:enabled] enabled
+/usr/bin/jq -s '.[0] * .[1]' \
+  <(echo '{"hhvm":{"enabled":true},"wordpress":{"woocommerce":true}}') \
+  <(cat /opt/local/amimoto.json) \
+  > /opt/local/amimoto.json
+
+/usr/bin/curl -L -s https://raw.githubusercontent.com/Launch-with-1-Click/lw1-amimoto/${AMIMOTO_BRANCH}/initial.wp-setup.sh | /bin/bash
