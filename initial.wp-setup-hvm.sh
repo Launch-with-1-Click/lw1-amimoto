5f36756bcb055589a4c21f04281b7d429daa720b
wokamoto <wokamoto1973@gmail.com>
2016-12-08 17:14:41 +0900

    added initial.wp-setup-hvm.sh (default php 7)

---
+++ initial.wp-setup-hvm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+AMIMOTO_BRANCH='2016.01'
+
+hash jq  || /usr/bin/yum -y install jq
+
+[ ! -e /opt/local ] && mkdir -p /opt/local
+[ ! -f /opt/local/amimoto.json ] && echo '{"run_list":["recipe[amimoto]"]}' > /opt/local/amimoto.json
+[ "$(cat /opt/local/amimoto.json)" = "" ] && echo '{"run_list":["recipe[amimoto]"]}' > /opt/local/amimoto.json
+
+# node[:phpfpm][:version] 70
+TMP_JSON=$(mktemp)
+/usr/bin/jq -s '.[0] * .[1]' \
+  <(echo '{"phpfpm":{"version":"70"}}') \
+  /opt/local/amimoto.json \
+  > ${TMP_JSON}
+[ -f ${TMP_JSON} ] && mv -f ${TMP_JSON} /opt/local/amimoto.json
+
+/usr/bin/curl -L -s https://raw.githubusercontent.com/Launch-with-1-Click/lw1-amimoto/${AMIMOTO_BRANCH}/initial.wp-setup.sh | /bin/bash
