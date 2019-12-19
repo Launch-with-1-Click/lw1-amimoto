7f48961a99bcda517dcabe4e4404f66e626c9103
wokamoto <wokamoto@digitalcube.jp>
2019-12-19 15:51:00 +0900

    Update WordPress 5.3.2

--- attributes/03_php.rb
+++ attributes/03_php.rb
@@ -72,9 +72,7 @@ default[:php][:packages] = %w{
   ImageMagick
   php-pecl-imagick
   }
-if node[:phpfpm][:version] >= '72'
-  default[:php][:packages].push('php-pecl-mcrypt')
-else
+if node[:phpfpm][:version] < '72'
   default[:php][:packages].push('php-mcrypt')
 end
 
